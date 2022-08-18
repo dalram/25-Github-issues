8e5ea3689bc451d7ba31b6e205f5a41cfa15e50e
Dalius Ramonas <93115669+dalram@users.noreply.github.com>
2022-08-18 11:56:11 +0300

    jest packages

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: [
+      "module:metro-react-native-babel-preset",
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
+    ],
     plugins: [
       [
         "module-resolver",
