cb7c03cfb08bee6cf0e75b6617692a25aa9c1afc
Dalius Ramonas <93115669+dalram@users.noreply.github.com>
2022-09-09 13:39:42 +0300

    BackHandler corrections

--- src/hooks/useBackHandlerAndroid.tsx
+++ src/hooks/useBackHandlerAndroid.tsx
@@ -1,27 +1,31 @@
 import { useEffect } from "react";
 import { Alert, BackHandler } from "react-native";
 
-const useBackHandlerAndroid = () => {
-  useEffect(() => {
-    const backAction = () => {
-      Alert.alert(
-        "Wait a second!",
-        "Are you sure you want to exit Github issues App?",
-        [
-          {
-            text: "Cancel",
-            onPress: () => null,
-            style: "cancel",
-          },
-          { text: "EXIT", onPress: () => BackHandler.exitApp() },
-        ]
-      );
-      return true;
-    };
-    BackHandler.addEventListener("hardwareBackPress", backAction);
-    return () =>
-      BackHandler.removeEventListener("hardwareBackPress", backAction);
-  }, []);
+const onBackAction = () => {
+  Alert.alert(
+    "Wait a second!",
+    "Are you sure you want to exit Github issues App?",
+    [
+      {
+        text: "Cancel",
+        onPress: () => null,
+        style: "cancel",
+      },
+      { text: "EXIT", onPress: () => BackHandler.exitApp() },
+    ]
+  );
+  return true;
 };
+
+const useBackHandlerAndroid = (stopBubblingUp = true): void =>
+  useEffect(() => {
+    const onPressBack = (): boolean => {
+      onBackAction();
+      return stopBubblingUp;
+    };
+    BackHandler.addEventListener("hardwareBackPress", onPressBack);
+    return () =>
+      BackHandler.removeEventListener("hardwareBackPress", onPressBack);
+  }, []);
 
 export default useBackHandlerAndroid;
